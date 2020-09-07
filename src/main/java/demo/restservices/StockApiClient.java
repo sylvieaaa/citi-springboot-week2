3674b2d027d1835a82ff3f3ea8400b43eeb0d732
bryanwongweiheng <bryanwongweiheng@gmail.com>
2020-09-07 09:34:56 +0800

    move external api base url to application.properties move external api token to an environment variable

--- src/main/java/demo/restservices/StockApiClient.java
+++ src/main/java/demo/restservices/StockApiClient.java
@@ -1,5 +1,6 @@
 package demo.restservices;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -8,10 +9,14 @@ import java.util.Arrays;
 
 @Component
 public class StockApiClient {
-    private String token = "add token here"; // TODO probably change to an environment variable
-    private String baseAddress = "https://finnhub.io/api/v1/quote"; // TODO move to application.properties
+    @Value("${spring.datasource.token}")
+    private String token;
+    @Value("${spring.datasource.baseurl}")
+    private String baseAddress;
 
     public double getDayReturn(String ticker) {
+        System.out.println(token);
+        System.out.println(baseAddress);
         RestTemplate template = new RestTemplate();
         String url = baseAddress + "?symbol=" + ticker.toUpperCase() + "&token=" + token;
         System.out.println(url);
