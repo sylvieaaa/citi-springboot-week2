3ad962881071d018bff7173f65ebaf41ac844805
bryanwongweiheng <bryanwongweiheng@gmail.com>
2020-09-11 16:16:01 +0800

    change jar file, remove statement from js file

--- mongodbsetup.js
+++ mongodbsetup.js
@@ -1,3 +1,2 @@
-use sp500
 db.User.insert({'password': "pass2234", 'name':'Sylvia', 'emailAddress': 'sylvia_s@gmail.com'})
 db.User.insert({'password': "123456", 'name':'Yi Jie', 'emailAddress': 'yijie@gmail.com'})
