1b5e90affa547795dec35b023ed5f0abd650439b
LinBeiYi <LINB0009@e.ntu.edu.sg>
2020-09-11 03:31:44 +0000

    Used Lombok Library to generate getter, setter and toString

--- src/main/java/demo/restservices/Quote.java
+++ src/main/java/demo/restservices/Quote.java
@@ -1,10 +1,12 @@
 package demo.restservices;
 
+import lombok.*;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Quote {
+public @ Data class Quote {
     @JsonProperty("c")
     private double current;
     @JsonProperty("h")
@@ -18,6 +20,7 @@ public class Quote {
     @JsonProperty("t")
     private int time;
 
+    /*
     public double getCurrent() {
         return current;
     }
@@ -73,4 +76,6 @@ public class Quote {
                 ", time='" + time + '\'' +
                 '}';
     }
+
+     */
 }
