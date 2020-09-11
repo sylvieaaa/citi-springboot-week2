77f56fb6cecf448f593d0ce4e19a32e364e8939a
LinBeiYi <LINB0009@e.ntu.edu.sg>
2020-09-11 06:59:06 +0000

    updated test case

--- src/main/java/demo/restservices/Application.java
+++ src/main/java/demo/restservices/Application.java
@@ -1,5 +1,6 @@
 package demo.restservices;
 
+import demo.restservices.mongodb.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/demo/restservices/mongodb/UserService.java
+++ src/main/java/demo/restservices/mongodb/UserService.java
@@ -3,10 +3,21 @@ package demo.restservices.mongodb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class UserService {
     @Autowired
     UserRepository userRepository;
+
+    @PostConstruct
+    public void init() {
+        userRepository.save(new User("0001", "beiyi", "beiyi@gmail.com", "0001"));
+        userRepository.save(new User("0002", "bryan", "bryan@gmail.com", "0002"));
+        userRepository.save(new User("0003", "sylvia", "sylvia@gmail.com", "0003"));
+        userRepository.save(new User("0004", "yijie", "yijie@gmail.com", "0004"));
+    }
 
     public User createUser(String password, String name, String emailAddress) {
         try {
@@ -37,12 +48,21 @@ public class UserService {
         1. If the entity with the given id is not available in collection in Mongo database, then save() will work same as insert method of MongoRepository and inserts the entity.
         2. If the entity with the given id is already there in collection in Mongo database, then save() method will update the entity.
         https://www.concretepage.com/spring-5/spring-data-mongorepository-update
+
      */
+
+
     public User updateEmailAddress(String password, String name, String emailAddress, String userId) {
         User user = new User(password, name, emailAddress, userId);
         userRepository.save(user);
         return userRepository.getUserById(userId);
     }
+
+
+
+    @PreDestroy
+    public void cleanup() {
+        userRepository.deleteAll(); }
 
 
 }
