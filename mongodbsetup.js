838fa20c8d26c41a09ce0d3d7349140fb6cb4d7c
sylvieeea <sylxswee@gmail.com>
2020-09-13 12:42:12 +0000

    docker compose edit

--- mongodbsetup.js
+++ mongodbsetup.js
@@ -1,2 +1,2 @@
 db.User.insert({'password': "pass2234", 'name':'Sylvia', 'emailAddress': 'sylvia_s@gmail.com'})
-db.User.insert({'password': "123456", 'name':'Yi Jie', 'emailAddress': 'yijie@gmail.com'})
+db.User.insert({'password': "123456", 'name':'Yi Jie', 'emailAddress': 'yi_jie@gmail.com'})
