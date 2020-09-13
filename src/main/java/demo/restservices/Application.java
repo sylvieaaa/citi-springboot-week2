fb1e86478f769533891095b2ffd64b40fdc7c5a6
See Yi Jie <cyberform.jys@gmail.com>
2020-09-14 00:05:57 +0800

    Fixed controller and S3Service

--- src/main/java/demo/restservices/Application.java
+++ src/main/java/demo/restservices/Application.java
@@ -11,7 +11,7 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         S3Service s3Service = ctx.getBean(S3Service.class);
-        s3Service.download("tick.svg", "/home/yijie/Desktop/tick.svg");
+//        s3Service.download("tick.svg", "/home/yijie/Desktop/tick.svg");
 //        s3Service.upload("Week-4-Hackathon.docx", "/home/yijie/Desktop/Week 4 Hackathon.docx");
     }
 }
